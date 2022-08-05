4e8435652a7ceaaf4f1542adc711ac084cc0eddb
patchwork01 <110390516+patchwork01@users.noreply.github.com>
2022-08-05 13:49:57 +0100

    Move environment modules under root module

--- terraform/environment/main.tf
+++ terraform/environment/main.tf
@@ -3,18 +3,18 @@ data "external" "current_ip" {
 }
 
 module "networking" {
-  source          = "../modules/networking"
+  source          = "modules/networking"
   namespace       = var.namespace
   external_ssh_ip = data.external.current_ip.result.ip
 }
 
 module "ssh-key" {
-  source    = "../modules/ssh-key"
+  source    = "modules/ssh-key"
   namespace = var.namespace
 }
 
 module "ec2" {
-  source     = "../modules/ec2"
+  source     = "modules/ec2"
   namespace  = var.namespace
   vpc        = module.networking.vpc
   sg_pub_id  = module.networking.sg_pub_id
